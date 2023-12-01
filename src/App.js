35ee8177544347fa60443e901eeb0de4934bb1dc
BikramNarayan <lcb2022029@iiitl.ac.in>
2023-12-02 01:06:37 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-
+// import { Link } from "react-feather";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Body />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/make" element={<Body />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Body /> */}
     </div>
   );
 }
